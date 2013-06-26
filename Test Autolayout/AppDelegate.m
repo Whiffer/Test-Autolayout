b34673ee486813d0143f45ddc7e32f14ee4728f9
Whiffer <Chuck@albatross.forethegreen.com.local>
2013-06-25 17:22:37 -0700

    Barebones test app.

--- Test Autolayout/AppDelegate.m
+++ Test Autolayout/AppDelegate.m
@@ -10,6 +10,8 @@
 
 @implementation AppDelegate
 {
+    IBOutlet NSView *mainView;
+    
     IBOutlet NSSplitView *outerSplitView;
     IBOutlet NSSplitView *innerSplitView;
 }
@@ -17,45 +19,11 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
-    //    [mainView setTranslatesAutoresizingMaskIntoConstraints:NO];
-    
-//    innerSplitView.delegate = self;
-//    outerSplitView.delegate = self;
 }
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
 {
     return YES;
 }
-
-//- (CGFloat)splitView:(NSSplitView *)splitView constrainMinCoordinate:(CGFloat)proposedCoordinate ofSubviewAt:(NSInteger)index
-//{
-//    if ([splitView isEqualTo:outerSplitView])
-//    {
-//        NSLog(@"%@:%ld minCoord:%0f", splitView.identifier, (long)index, proposedCoordinate);
-//        return proposedCoordinate + 100.0;
-//    }
-////    else if ([splitView isEqualTo:innerSplitView])
-////    {
-////        NSLog(@"%@:%ld minCoord:%0f", splitView.identifier, (long)index, proposedCoordinate);
-////        return proposedCoordinate + 50.0;
-////    }
-//    return proposedCoordinate;
-//}
-
-//- (CGFloat)splitView:(NSSplitView *)splitView constrainMaxCoordinate:(CGFloat)proposedCoordinate ofSubviewAt:(NSInteger)index
-//{
-//    if ([splitView isEqualTo:outerSplitView])
-//    {
-//        NSLog(@"%@:%ld maxCoord:%0f", splitView.identifier, (long)index, proposedCoordinate);
-//        return proposedCoordinate - 200.0;
-//    }
-////    else if ([splitView isEqualTo:innerSplitView])
-////    {
-////        NSLog(@"%@:%ld maxCoord:%0f", splitView.identifier, (long)index, proposedCoordinate);
-////        return proposedCoordinate - 50.0;
-////    }
-//    return proposedCoordinate;
-//}
 
 @end
